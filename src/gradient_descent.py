ee6fd96a1ad7db21715f4f160c773e682944e506
Mridul Kumar Rai <mridulkumarrai@users.noreply.github.com>
2020-10-27 20:10:08 +0530

    Added plotting functionality, some minor changes as well

--- src/gradient_descent.py
+++ src/gradient_descent.py
@@ -3,15 +3,17 @@ import pandas as pd
 import numpy as np
 import pre_processing
 from numpy.linalg import inv
-from matplotlib import pyplot
+import matplotlib
+import matplotlib.pyplot as plt
 
 def error(y,yi):
     return (np.mean((y-yi)**2))**(1/2)
 
-def gd(x, y_org, rate, num_iter):
+def gd(x, y_org, rate, num_iter, isPlot):
     '''
     Function to apply Gradient descent on training data, and find beta 
     '''
+    errorToPlot = []
     beta=np.ones((4,1))
     rate=rate/(x.shape[0])
     y_train=x.dot(beta)
@@ -21,13 +23,22 @@ def gd(x, y_org, rate, num_iter):
         beta=beta-rate*(x.T.dot(diff))
         diff=x.dot(beta)-y_org
         count=count+1
+        if isPlot == 1:
+            errorToPlot.append(error(y_org,x.dot(beta)))
         if(count%50==0):
             print(error(y_org,x.dot(beta)))
+    if isPlot == 1:
+        # plt.figure()
+        plt.plot(np.arange(num_iter), errorToPlot)
+        # plt.plot(np.arange(num_iter), errorToPlot, 'r', label='batch')
+        plt.xlabel('iteration')
+        plt.ylabel('RMSE')
+        # plt.show()
     print("\n")
     
     return beta
 
-def process(data, learning_rate, num_iter):
+def process(data, learning_rate, num_iter, isPlot):
     # Training data part
     (training_data,testing_data)=pre_processing.train_test_split(data)
     x=training_data[:,0:3]
@@ -36,7 +47,7 @@ def process(data, learning_rate, num_iter):
     x=np.append(ones,x,axis=1)
     y_org=np.reshape(y_org,(x.shape[0],1))
 
-    beta = gd(x, y_org, learning_rate, num_iter)
+    beta = gd(x, y_org, learning_rate, num_iter, isPlot)
     
     y_train = x.dot(beta)
     error_in_train = error(y_train,y_org)
@@ -60,13 +71,34 @@ def main():
 
     train_error=[] # A list of rmse for each model for training data
     test_error=[] # A list of rmse for each model for test data
+
+    isPlot = 0
     for i in range(0,20):
         print("Model " + str(i+1))
-        (error_in_train, error_in_test) = process(data, learning_rate, num_iter)
+        (error_in_train, error_in_test) = process(data, learning_rate, num_iter, isPlot)
         train_error.append(error_in_train)
         test_error.append(error_in_test)
+    
+    # Plot of RMSE vs iteration
+    plt.figure()
+    plt.subplot(2,2,1)
+    dummy = process(data, 0.1, num_iter,1)
+    plt.title('Learning Rate = 0.1')
 
+    plt.subplot(2,2,2)
+    dummy = process(data, 0.01, num_iter,1)
+    plt.title('Learning Rate = 0.01')
 
+    plt.subplot(2,2,3)
+    dummy = process(data, 0.001, num_iter,1)
+    plt.title('Learning Rate = 0.001')
+
+    plt.subplot(2,2,4)
+    dummy = process(data, 0.0001, num_iter,1)
+    plt.title('Learning Rate = 0.0001')
+
+    plt.show()
+    plt.close()
     train_error_mean=np.mean(train_error)
     train_error_var=np.var(train_error)
     train_error_min=min(train_error)
