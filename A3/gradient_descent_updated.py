7a95cc828a55cf5a5d747385b7cbaf77b3f2c9e3
Shashank <srirangamshashank@gmail.com>
2020-11-04 12:38:28 +0530

    Gradient Descent with regularisation

--- A3/gradient_descent_updated.py
+++ A3/gradient_descent_updated.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import PolynomialFeatures
 def error(y,yi):
     return (np.mean((y-yi)**2,dtype=np.float64))**(1/2)
 
-def gd(x, y_org, rate, num_iter,lam,Islasso):
+def gd(x, y_org, rate, num_iter,lam,Islasso,Isdegree):
     '''
     Function to apply Gradient descent on training data, and find beta 
     '''
@@ -21,13 +21,15 @@ def gd(x, y_org, rate, num_iter,lam,Islasso):
     count=0
     costs = []
     for j in range(0,num_iter):
-        if(Islasso):
+        if(Islasso==1 and Isdegree==1):
             #beta=beta - rate*((x.T).dot(diff))
             #beta=beta-rate*(x.T.dot(diff)) + lam*np.ones((x.shape[1],1))
-            beta=beta-rate*(x.T.dot(diff)) + (lam/(x.shape[0]))*np.sign(beta)
-        else:
+            beta=beta-rate*(x.T.dot(diff)) -(rate*(lam*(x.shape[0]))*np.sign(beta))
+        elif(Islasso==0 and Isdegree==1):
             #beta=beta-rate*(x.T.dot(diff))
-            beta=beta-rate*(x.T.dot(diff)+2*lam*beta)
+            beta=beta-rate*(x.T.dot(diff)-rate*2*lam*beta*x.shape[0])
+        else:
+            beta=beta - rate*((x.T).dot(diff))
         diff=x.dot(beta)-y_org
         count=count+1
         costs.append(error(y_org,x.dot(beta)))
@@ -36,54 +38,43 @@ def gd(x, y_org, rate, num_iter,lam,Islasso):
             print("{:.5f}".format(error(y_org,x.dot(beta))))
             
             
-    plt.plot(np.arange(num_iter), costs, label = 'λ = ' + "{:.2f}".format(lam))
+    #plt.plot(np.arange(num_iter), costs, label = 'λ = ' + "{:.2f}".format(lam))
     return beta
 
-def process(data, learning_rate, num_iter,degree,Islasso):
+def process(data, learning_rate, num_iter,degree,Islasso,Isdegree):
     # Training data part
     (training_data,validation_data,testing_data)=pre_processing.train_test_split(data,degree)
     x=training_data[:,:-1]
     y_org=training_data[:,-1]
     y_org=np.reshape(y_org,(x.shape[0],1))
-    error_in_train=[]
-    error_in_test=[]
-    error_in_validation=[]
     lambdas=[]
-
-    plt.figure()
-    plt.xlabel('Iterations')
-    plt.ylabel('Cost')
-    plt.title('Learning Rate = '+str(learning_rate) )
-    for j in np.arange(0.1, 1.0, 0.1):
-        '''
-        if(j==0):
-            lam=0
-            lambdas.append(lam)
-        else:
-            lam=random.random()
-            lambdas.append(lam)
-        '''
-        print("λ = " + "{:.2f}".format(j))
-        beta = gd(x, y_org, learning_rate, num_iter,j,1)
+    betas=[]
+    error_in_train=0
+    temp=[]
+    error_in_validation=0
+    error_in_test=0
+    for j in range(0,10):
+        lam=random.random()
+        lambdas.append(lam)
+        beta = gd(x, y_org, learning_rate, num_iter,lam,Islasso,Isdegree)
+        betas.append(beta)
         
-        y_train = x.dot(beta)
-        error_in_train.append(error(y_train,y_org))
-        
-        x_validate=validation_data[:,:-1]
-        y_validate_org=validation_data[:,-1]
+    x_validate=validation_data[:,:-1]
+    y_validate_org=validation_data[:,-1]
+    for beta in betas:
         y_validate=x_validate.dot(beta)
-        error_in_validation.append(error(y_validate_org,y_validate))
-        # Test data part
-        x_test=testing_data[:,:-1]
-        y_test_org=testing_data[:,-1]
+        temp.append(error(y_validate_org,y_validate))
+    min_index=temp.index(min(temp))
+    min_lambda=lambdas[min_index]
+    min_beta=betas[min_index]
     
-        y_test=x_test.dot(beta)
-        error_in_test.append(error(y_test,y_test_org))
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
-    plt.close()
-    return error_in_train,error_in_validation,error_in_test,lambdas 
+    error_in_train=error(x.dot(min_beta),y_org)
+    error_in_validation=error(x_validate.dot(min_beta),y_validate_org)
+    # Test data part
+    x_test=testing_data[:,:-1]
+    y_test=testing_data[:,-1]
+    error_in_test=error(x_test.dot(min_beta),y_test)
+    return min_lambda,min_beta,error_in_train,error_in_validation,error_in_test
 
 def main():
     data=pd.read_csv("insurance.txt").to_numpy()
@@ -94,15 +85,44 @@ def main():
     all_train_error=[] # A list of rmse for each model for training data
     all_test_error=[] # A list of rmse for each model for test data
     all_validation_error=[]
+    all_errors=[]
     Islasso=1
-    for i in range(7,8):
+    for i in range(0,10):
         print("Model " + str(i+1))
-        (error_in_train,error_in_validation,error_in_test,lambdas) = process(data, learning_rate, num_iter,i+1,Islasso)
-        all_train_error.append(error_in_train)
-        all_validation_error.append(error_in_validation)
-        all_test_error.append(error_in_test)
-        print(all_train_error)
-        
+        lambdas,beta,error_in_train,error_in_validation,error_in_test= process(data, learning_rate, num_iter,i+1,Islasso,1)
+        all_errors.append([i+1,lambdas,error_in_train,error_in_validation,error_in_test])
+    all_errors=np.array(all_errors)
+    df1=pd.DataFrame({'Degree':all_errors[:,0],'lambda':all_errors[:,1],'train-error':all_errors[:,2],'validation-error':all_errors[:,3],'test-error':all_errors[:,4]})
+    
+    
+    all_errors=[]
+    Islasso=0
+    for i in range(0,10):
+        print("Model " + str(i+1))
+        lambdas,beta,error_in_train,error_in_validation,error_in_test= process(data, learning_rate, num_iter,i+1,Islasso,1)
+        all_errors.append([i+1,lambdas,error_in_train,error_in_validation,error_in_test])
+    all_errors=np.array(all_errors)
+    df2=pd.DataFrame({'Degree':all_errors[:,0],'lambda':all_errors[:,1],'train-error':all_errors[:,2],'validation-error':all_errors[:,3],'test-error':all_errors[:,4]})
+    
+    
+    all_errors=[]
+    for i in range(0,10):
+        print("Model " + str(i+1))
+        lambdas,beta,error_in_train,error_in_validation,error_in_test= process(data, learning_rate, num_iter,i+1,Islasso,0)
+        all_errors.append([i+1,0,error_in_train,error_in_validation,error_in_test])
+    all_errors=np.array(all_errors)
+    df3=pd.DataFrame({'Degree':all_errors[:,0],'lambda':all_errors[:,1],'train-error':all_errors[:,2],'validation-error':all_errors[:,3],'test-error':all_errors[:,4]})
+    
+    print("Lasso Regression With Regularisation")
+    print(df1)
+    print("Ridge Regression With Regularisation")
+    print(df2)
+    print("No Regularisation")
+    print(df3)
+    
+    
+    
+    
     
 """
     train_error_mean=np.mean(train_error)
